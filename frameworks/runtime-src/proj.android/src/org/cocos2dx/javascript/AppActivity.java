628b8254e41c52999e028eeea31f70ca33416c48
bokuweb <info@typebeats.com>
2015-04-12 14:06:57 +0900

    add ad

--- frameworks/runtime-src/proj.android/src/org/cocos2dx/javascript/AppActivity.java
+++ frameworks/runtime-src/proj.android/src/org/cocos2dx/javascript/AppActivity.java
@@ -29,16 +29,23 @@ package org.cocos2dx.javascript;
 import org.cocos2dx.lib.Cocos2dxActivity;
 import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
 import jp.co.imobile.sdkads.android.ImobileSdkAd;
+import jp.co.imobile.sdkads.android.ImobileSdkAdListener;
 import android.os.Bundle;
 import android.widget.FrameLayout;
 import android.widget.FrameLayout.LayoutParams;
 import android.view.Gravity;
+import android.view.KeyEvent; 
 import android.R;
 
 public class AppActivity extends Cocos2dxActivity {
     static final String IMOBILE_BANNER_PID="32640";
     static final String IMOBILE_BANNER_MID="161737";
     static final String IMOBILE_BANNER_SID="429126";
+
+    static final String IMOBILE_INTERSTITIAL_PID = "32640";
+    static final String IMOBILE_INTERSTITIAL_MID = "161737";
+    static final String IMOBILE_INTERSTITIAL_SID = "435871";
+
 	@Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +53,11 @@ public class AppActivity extends Cocos2dxActivity {
         ImobileSdkAd.registerSpotInline(this, IMOBILE_BANNER_PID, IMOBILE_BANNER_MID, IMOBILE_BANNER_SID);
         // 広告の取得を開始します
         ImobileSdkAd.start(IMOBILE_BANNER_SID);
+        
+        // スポット情報を設定します
+        ImobileSdkAd.registerSpotFullScreen(this, IMOBILE_INTERSTITIAL_PID, IMOBILE_INTERSTITIAL_MID, IMOBILE_INTERSTITIAL_SID);
+        // 広告の取得を開始します
+        ImobileSdkAd.start(IMOBILE_INTERSTITIAL_SID);
         
         // 広告を表示するViewを作成します
         FrameLayout imobileAdLayout = new FrameLayout(this);
@@ -57,6 +69,25 @@ public class AppActivity extends Cocos2dxActivity {
         // 広告を表示します
         ImobileSdkAd.showAd(this, IMOBILE_BANNER_SID, imobileAdLayout);
     }
+
+    //戻るボタン（Backキー）押下で広告を表示
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            //3. 広告表示
+            ImobileSdkAd.showAdForce(this, IMOBILE_INTERSTITIAL_SID, new ImobileSdkAdListener() {
+                //4.広告が閉じられた場合（閉じるボタン、もしくはクリックボタンがタップされた場合）
+                @Override
+                public void onAdCloseCompleted() {
+                    //5.広告が閉じられた場合、Activityを終了させます
+                    AppActivity.this.finish();
+                }
+            });
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
     @Override
     protected void onDestroy() {
         // Activity廃棄時の後処理
